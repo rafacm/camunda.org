4cc548db2920b570416c85472bc0a6682e56bb54
Daniel Meyer <daniel.meyer@camunda.com>
2013-02-06 17:26:16 +0100

    added infrastructure for bpmn implementation docs

--- app/assets/js/app/app.js
+++ app/assets/js/app/app.js
@@ -1,7 +1,14 @@
 'use strict';
 
 angular
-  .module('camundaorg', ['ng', 'camundaorg.filters', 'camundaorg.services', 'camundaorg.directives']);
+  .module('camundaorg', ['ng', 'camundaorg.filters', 'camundaorg.services', 'camundaorg.directives'])
+
+  .config(function ($routeProvider) {
+        $routeProvider.when('/events/start-events', {
+            templateUrl: 'partials/implement/events/start-events.html',
+            controller: 'DefaultController'
+        });
+    });
 
 
     
