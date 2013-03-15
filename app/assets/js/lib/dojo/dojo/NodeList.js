cd66fc60afca0a48c264cc610d3a7cd4a15fe776
Daniel Meyer <daniel.meyer@camunda.com>
2013-03-15 23:42:57 +0100

    chore(dojo) using minified libs

--- app/assets/js/lib/dojo/dojo/NodeList.js
+++ app/assets/js/lib/dojo/dojo/NodeList.js
@@ -5,6 +5,6 @@
 */
 
 //>>built
-define("dojo/errors/RequestTimeoutError",["./create","./RequestError"],function(_1,_2){
-return _1("RequestTimeoutError",null,_2,{dojoType:"timeout"});
+define("dojo/NodeList",["./query"],function(_1){
+return _1.NodeList;
 });
