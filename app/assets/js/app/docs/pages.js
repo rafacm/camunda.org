b15bcbcf481448d1ce258f897941a6e00d11a377
Bernd Ruecker <bernd.ruecker@camunda.com>
2013-03-16 21:42:08 +0100

    added migration guide stub

--- app/assets/js/app/docs/pages.js
+++ app/assets/js/app/docs/pages.js
@@ -35,9 +35,10 @@ CAM_PAGES = [
   // engine api documentation
   { section: "implementation-java", category: "Introduction", id: "impl-java-engine-bootstrap", name: "Startup and Configuration Options", url: "/engine/configuration-and-startup", keywords: "engine startup configuration bootstrap overview ProcessEngineConfiguration" },
   { section: "implementation-java", category: "Introduction", id: "impl-java-engine-bootstrap", name: "Container Integration", url: "/engine/container-integration", keywords: "container integration jboss glassfish tomcat" },
-  { section: "implementation-java", category: "Introduction", id: "impl-concepts", name: "Process Applications", url: "/engine/process-application", keywords: "process application" },
-  { section: "implementation-java", category: "Introduction", id: "impl-concepts", name: "Important Concepts", url: "/engine/concepts", keywords: "concepts" },
-  
+  { section: "implementation-java", category: "Introduction", id: "impl-pa", name: "Process Applications", url: "/engine/process-application", keywords: "process application" },
+  { section: "implementation-java", category: "Introduction", id: "impl-concepts", name: "Important Concepts", url: "/engine/concepts", keywords: "concepts" },  
+  { section: "implementation-java", category: "Introduction", id: "impl-activiti-migration", name: "Activiti Migration Guide", url: "/engine/activiti-migration-guide", keywords: "activiti migration fox" },
+
   { section: "implementation-java", category: "Programming", id: "impl-java-services", name: "Services", url: "/engine/services", keywords: "services api" },
   { section: "implementation-java", category: "Programming", id: "impl-java-query-api", name: "Query API", url: "/engine/query-api", keywords: "query api" },
   { section: "implementation-java", category: "Programming", id: "impl-java-expressions", name: "Expression Language", url: "/engine/expressions", keywords: "expression language el uel juel" },
